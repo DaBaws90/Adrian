4782086c7c04fb4ffa5a818cf9d7310284a4bb0d
DaBaws-Laptop <DaBaws-Laptop@DESKTOP-FHJIRU6>
2018-10-18 14:08:53 +0200

    Version 0.1 - First entity created. Directories structure generated

---
+++ src/main/java/com/example/Adrian/AdrianApplication.java
@@ -0,0 +1,12 @@
+package com.example.Adrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrianApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/Adrian/entity/Coches.java
@@ -0,0 +1,108 @@
+package com.example.Adrian.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="coches")
+public class Coches {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="matricula")
+	private String matricula;
+	
+	@Column(name="marca")
+	private String marca;
+	
+	@Column(name="modelo")
+	private String modelo;
+	
+	@Column(name="color")
+	private String color;
+	
+	@Column(name="potencia")
+	private int potencia;
+	
+	@Column(name="foto")
+	private String foto;
+	
+	public Coches() {
+		
+	}
+
+	public Coches(int id, String matricula, String marca, String modelo, String color, int potencia, String foto) {
+		super();
+		this.id = id;
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.color = color;
+		this.potencia = potencia;
+		this.foto = foto;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+
+}
