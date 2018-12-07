b5adc53410ed0ffcc561f105f0e0c615e178eda5
DaBaws90 <43435682+DaBaws90@users.noreply.github.com>
2018-12-07 18:24:26 +0100

    Simulating security (Login and Logout)

---
+++ src/main/java/com/example/Adrian/controller/LoginController.java
@@ -0,0 +1,45 @@
+package com.example.Adrian.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.example.Adrian.model.UserCredentials;
+
+@Controller
+public class LoginController {
+	private static final String LOGIN_VIEW = "loginView";
+
+	@GetMapping("/")
+	public RedirectView redirect() {
+		return new RedirectView("/login");
+	}
+	
+	@GetMapping("/login")
+	public ModelAndView signin(@RequestParam(name="error", required=false) String error, @RequestParam(name="logout", required=false) String logout) {
+		ModelAndView mav = new ModelAndView(LOGIN_VIEW);
+		mav.addObject("userCredentials", new UserCredentials());
+		mav.addObject("error", error);
+		mav.addObject("logout", logout);
+		return mav;
+	}
+	
+	@PostMapping("signin")
+	public ModelAndView loginCheck(@ModelAttribute("userCredentials") UserCredentials userCredentials, RedirectAttributes redirectAttributes) {
+		ModelAndView mav = new ModelAndView();
+		if(userCredentials.getUsername().equals("user") && userCredentials.getPassword().equals("user")) {
+			mav.setViewName("redirect:/home/");
+			//redirectAttributes.addFlashAttribute("error", 0);
+		}
+		else {
+			mav.setViewName("redirect:/login?error");
+			//redirectAttributes.addFlashAttribute("error", 1);
+		}
+		return mav;
+	}
+}
